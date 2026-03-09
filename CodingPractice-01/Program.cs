cb86bc67839885d1bc465971033301889febd8c2
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-09 18:01:57 +0900

    뭔가 이상

--- CodingPractice-01/Program.cs
+++ CodingPractice-01/Program.cs
@@ -1,4 +1,14 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("'''");
+{
+    SayDelegate sayhi = Hello;
+
+    sayhi();
+    
+    delegate void SayDelegate();
+
+    static void Hello() => Console.WriteLine("Hello Delegate");
+}
+Console.WriteLine("'''\n");
