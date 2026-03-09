da3d6563a9b2be417af2eac39e7e805b748f93d5
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-10 01:23:45 +0900

    연습 3 완료

--- CodingPractice-03/Program.cs
+++ CodingPractice-03/Program.cs
@@ -1,4 +1,29 @@
 using System;
 
 // README.md를 읽고 아래에 코드를 작성하세요.
-Console.WriteLine("코드를 작성하세요.");
+Console.WriteLine("'''");
+{
+    Action driver = GoForward;
+    driver += GoLeft;
+    driver += GoRight;
+    driver();
+
+    
+}
+Console.WriteLine("'''\n");
+
+Console.WriteLine("'''");
+{
+    Action driver = GoForward;
+    driver += GoLeft;
+
+    driver += delegate ()
+    {
+        Console.WriteLine("우회전");
+    };
+}
+Console.WriteLine("'''\n");
+
+static void GoForward() => Console.WriteLine("직진");
+static void GoLeft() => Console.WriteLine("좌회전");
+static void GoRight() => Console.WriteLine("우회전");
