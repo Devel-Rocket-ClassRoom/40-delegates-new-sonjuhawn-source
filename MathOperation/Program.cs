4a118f3e0f52b05e1a7fde6ee9411882f23fc176
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-10 09:20:38 +0900

    사칙연산 대리자

--- MathOperation/Program.cs
+++ MathOperation/Program.cs
@@ -2,3 +2,32 @@ using System;
 
 // README.md를 읽고 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
+
+MathOp op = Add;
+
+Console.WriteLine("[덧셈]");
+Console.WriteLine($"20 + 4 = {op(20,4)}");
+op -= Add;
+
+Console.WriteLine("[뺄셈]");
+op = Subtract;
+Console.WriteLine($"20 - 4 = {op(20, 4)}");
+op -= Subtract;
+
+Console.WriteLine("[곱셈]");
+op = Multiply;
+Console.WriteLine($"20 * 4 = {op(20, 4)}");
+op -= Multiply;
+
+Console.WriteLine("[나눗셈]");
+op = Divide;
+Console.WriteLine($"20 / 4 = {op(20, 4)}");
+
+
+int Add(int a, int b) => a + b;
+int Subtract(int a, int b) => a - b;
+int Multiply(int a, int b) => a * b;
+int Divide(int a, int b) => a / b;
+
+delegate int MathOp(int x, int y);
+
