b08e8367eadb73277cf6a85db2c16b95d5509e62
Son-JuHwan <sonjuhawn@gmail.com>
2026-03-10 17:34:06 +0900

    Update Program.cs

--- MulticastDelegate/Program.cs
+++ MulticastDelegate/Program.cs
@@ -3,4 +3,25 @@ using System;
 // README.md를 읽고 아래에 코드를 작성하세요.
 Console.WriteLine("코드를 작성하세요.");
 
-Action<string> PrintOriginal() = (msg) => 
+Action<string> print = ();
+
+void PrintOriginal(string str)
+{
+    Console.WriteLine($"원본: {str}");
+}
+void PrintUpper(string str)
+{
+    Console.WriteLine($"대문자: { str.ToUpper()}");
+}
+void PrintLower(string str)
+{
+    Console.WriteLine($"소문자: {str.ToLower()}");
+}
+void PrintLength(string str)
+{
+    Console.WriteLine($"길이: {str.Length}");
+}
+
+
+Action<string> print = (msg) => 
+
